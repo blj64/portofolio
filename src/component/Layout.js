ef09f1f7087ef3f642d7797ab5ce867a66035519
blj64 <jliogerbun@gmail.com>
2024-03-19 13:09:29 +0200

    portofolio - react - tailwind

---
+++ src/component/Layout.js
@@ -0,0 +1,10 @@
+const Layout = () => {
+    return (
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>   
+            
+        )
+}
+
+export default Layout
